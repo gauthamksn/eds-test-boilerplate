01a272c2365cf6a3f3ae60d5c1e24785eecbd131
gauthamksn <gsnair@adobe.com>
2025-05-15 15:20:15 +0530

    plugin try 4

--- tools/sidekick/plugins/test/test.js
+++ tools/sidekick/plugins/test/test.js
@@ -1,11 +1,12 @@
-const container = `<sp-split-view primary-size="350" dir="ltr" resizable="" splitter-pos="350">
+
+export async function decorate(container, data, query) {
+    const container = `<sp-split-view primary-size="350" dir="ltr" resizable="" splitter-pos="350">
       <div class="menu"><sp-button>Try me</sp-button></div>>
       <div class="content"><sp-button>Test Infos</sp-button></div>
 </sp-split-view>`
 
-export async function decorate(container, data, query) {
     console.log('test plugin loaded', data, query);
-    container.innerHTML = container
+    container.innerHTML = container;
 }
 
 export default {
