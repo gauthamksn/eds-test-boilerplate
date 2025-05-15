099fb3bcabb8b9fc8b7bf4ca7f947222d46cf43f
gauthamksn <gsnair@adobe.com>
2025-05-15 15:07:36 +0530

    Plugin try 2

--- tools/sidekick/plugins/test/test.js
+++ tools/sidekick/plugins/test/test.js
@@ -1,4 +1,9 @@
-export function decorate(container, data) {
-    console.log('test plugin loaded', data);
+export async function decorate(container, data, query) {
+    console.log('test plugin loaded', data, query);
     container.innerHTML = "<sp-button>Try me</sp-button>"
+}
+
+export default {
+    title: 'Visual Tests',
+    searchEnabled: true,
 }
